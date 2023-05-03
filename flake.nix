2caad72553f52666641ef31542b88cddfbf8c7fe
Zhaofeng Li <hello@zhaofeng.li>
2023-05-03 03:11:05 +0000

    [not for upstream] Add Nix environment

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "NaCl on ARM";
+  description = "A simple project";
 
   inputs = {
     mars-std.url = "github:mars-research/mars-std";
@@ -7,44 +7,23 @@
 
   outputs = { self, mars-std, ... }: let
     # System types to support.
-    supportedSystems = [ "x86_64-linux" "aarch64-linux" ];
+    supportedSystems = [ "aarch64-linux" ];
   in mars-std.lib.eachSystem supportedSystems (system: let
     pkgs = mars-std.legacyPackages.${system};
-    armNativePkgs = mars-std.legacyPackages.aarch64-linux;
-    armPkgs = pkgs.pkgsCross.aarch64-multiplatform;
-
-    llvmSelected = pkgs.llvmPackages_14;
-
-    mkShell = pkgs.mkShell.override {
-      stdenv = llvmSelected.stdenv;
-    };
   in {
-    devShell = mkShell {
-      inputsFrom = [ llvmSelected.llvm ];
-
-      # cmake -B build $cmakeFlags
-      cmakeFlags = [
-        "-Sllvm"
-        "-DLLVM_ENABLE_PROJECTS='clang;lld'"
-        "-DCMAKE_BUILD_TYPE=Debug"
-        "-DLLVM_ENABLE_LIBCXX=TRUE"
-        "-GNinja"
-      ];
-
-      ARM_CFLAGS = let
-        gcc = armNativePkgs.stdenv.cc.cc;
-        linuxArch = armNativePkgs.stdenv.targetPlatform.linuxArch;
-        platform = armNativePkgs.stdenv.targetPlatform.config;
-        gccPath = "${gcc}/lib/gcc/${platform}/${gcc.version}";
-        libc = armNativePkgs.glibc;
-      in "--target=aarch64-pc-linux -I ${libc.dev}/include -L ${gccPath} -B ${gccPath} -L ${libc}/lib -B ${libc}/lib -Wl,-dynamic-linker,${libc}/lib/ld-linux-aarch64.so.1";
-
-      #LLD_UNWRAPPED = "${pkgs.llvmPackages_14.lld}/bin/ld.lld";
-
+    devShell = pkgs.mkShell {
       nativeBuildInputs = with pkgs; [
-        # Insert dev dependencies here
-        armPkgs.stdenv.cc
-        qemu
+        clang cmake meson ninja python3
+      ];
+      cmakeFlags = [
+        "-GNinja"
+        "-DGCC_INSTALL_PREFIX=${pkgs.gcc-unwrapped}"
+        "-DC_INCLUDE_DIRS=${pkgs.stdenv.cc.libc.dev}/include"
+        "-DPREFIX_DIRS=${pkgs.stdenv.cc.libc}/lib"
+        "-DLLVM_ENABLE_PROJECTS='clang;lld'"
+        "-DLLVM_ENABLE_RUNTIMES=openmp"
+        "-DCMAKE_BUILD_TYPE=MinSizeRel"
+        "-DLLVM_TARGETS_TO_BUILD=X86"
       ];
     };
   });
